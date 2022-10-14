43b8d11d8e6906fff3e0e22b9645c0ed9674f260
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-14 18:04:26 +0100

    Add MenuListViewModelTests file

---
+++ Albertos/AlbertosTests/Menu/Presentation/MenuListViewModelTests.swift
@@ -0,0 +1,10 @@
+//  Created on 14.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import XCTest
+
+final class MenuListViewModelTests: XCTestCase {
+
+    func testCallsGivenGroupingFunction() { }
+
+}
