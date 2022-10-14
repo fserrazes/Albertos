04250740b5c08ae85c42e38abdfab3c61b122f2b
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-14 11:26:14 +0100

    Add MenuGroupingTests file with the Test list

---
+++ AlbertosCore/AlbertosCoreTests/MenuGroupingTests.swift
@@ -0,0 +1,19 @@
+//  Created on 14.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import XCTest
+
+final class MenuGroupingTests: XCTestCase {
+
+    func test_menuWithManyCategories_returnsOneSectionPerCategory() {
+        
+    }
+    
+    func test_menuWithOneCategory_returnsOneSection() {
+        
+    }
+    
+    func test_emptyMenu_returnsEmptySections() {
+        
+    }
+}
