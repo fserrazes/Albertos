5204952b3c8a7ec811076d9489096d4e9ba8f5b9
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-14 11:01:10 +0100

    Initial Commit

---
+++ AlbertosCore/AlbertosCore.h
@@ -0,0 +1,14 @@
+//  Created on 14.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlbertosCore.
+FOUNDATION_EXPORT double AlbertosCoreVersionNumber;
+
+//! Project version string for AlbertosCore.
+FOUNDATION_EXPORT const unsigned char AlbertosCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlbertosCore/PublicHeader.h>
+
+
