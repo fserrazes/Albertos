c5a4b1a40912f8b4f747ba384cbdf4c04cb7e600
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-14 16:17:24 +0100

    Add MenuRowViewModelTests file with Test list

---
+++ Albertos/AlbertosTests/Menu/Presentation/MenuRowViewModelTests.swift
@@ -0,0 +1,12 @@
+//  Created on 14.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import XCTest
+
+final class MenuRowViewModelTests: XCTestCase {
+
+    func test_whenItemIsNotSpicy_textIsItemNameOnly() {}
+    
+    func test_whenItemIsSpicy_textIsItemWithChillyEmoji() {}
+
+}
