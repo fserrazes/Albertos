ce3be112f89b2f0693deeaaf549239cd433139c2
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-17 16:21:14 +0100

    Add MenuItemDetailViewModelTests file with Test list

---
+++ Albertos/AlbertosTests/Menu Item Detail/Presentation/MenuItemDetailViewModelTests.swift
@@ -0,0 +1,24 @@
+//  Created on 17.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import XCTest
+
+final class MenuItemDetailViewModelTests: XCTestCase {
+
+    func test_whenItemIsInOrder_ButtonSaysRemove() {
+        
+    }
+    
+    func test_whenItemIsNotInOrder_ButtonSaysAdd() {
+        
+    }
+    
+    func test_whenItemIsInOrder_ButtonActionRemovesIt() {
+        
+    }
+    
+    func test_whenItemIsNotInOrder_ButtonActionAddsIt() {
+        
+    }
+
+}
