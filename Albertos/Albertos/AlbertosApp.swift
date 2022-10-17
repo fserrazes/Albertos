061511ba3323e9d465fa6f04e719e76c05fc7e88
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-17 15:30:38 +0100

    Move hard-code menu to previews content folder

--- Albertos/Albertos/AlbertosApp.swift
+++ Albertos/Albertos/AlbertosApp.swift
@@ -4,18 +4,6 @@
 import SwiftUI
 import AlbertosCore
 
-// In this first iteration the menu is an hard-coded array
-public let menu = [
-    MenuItem(category: "starters", name: "Caprese Salad", spicy: false, price: 3.45),
-    MenuItem(category: "starters", name: "Arancini Balls", spicy: false, price: 4.56),
-    MenuItem(category: "pastas", name: "Penne all'Arrabbiata", spicy: true, price: 8.90),
-    MenuItem(category: "pastas", name: "Spaghetti Carbonara", spicy: false, price: 9.10),
-    MenuItem(category: "drinks", name: "Water", spicy: false, price: 2.34),
-    MenuItem(category: "drinks", name: "Red Wine", spicy: false, price: 3.45),
-    MenuItem(category: "desserts", name: "Tiramisù", spicy: false, price: 5.43),
-    MenuItem(category: "desserts", name: "Crema Catalana", spicy: false, price: 4.68),
-]
-
 @main
 struct AlbertosApp: App {
     
---
+++ Albertos/Albertos/Preview Content/DataModel.swift
@@ -0,0 +1,16 @@
+//  Created on 17.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import Foundation
+import AlbertosCore
+
+public let menu = [
+    MenuItem(category: "starters", name: "Caprese Salad", spicy: false, price: 3.45),
+    MenuItem(category: "starters", name: "Arancini Balls", spicy: false, price: 4.56),
+    MenuItem(category: "pastas", name: "Penne all'Arrabbiata", spicy: true, price: 8.90),
+    MenuItem(category: "pastas", name: "Spaghetti Carbonara", spicy: false, price: 9.10),
+    MenuItem(category: "drinks", name: "Water", spicy: false, price: 2.34),
+    MenuItem(category: "drinks", name: "Red Wine", spicy: false, price: 3.45),
+    MenuItem(category: "desserts", name: "Tiramisù", spicy: false, price: 5.43),
+    MenuItem(category: "desserts", name: "Crema Catalana", spicy: false, price: 4.68),
+]
