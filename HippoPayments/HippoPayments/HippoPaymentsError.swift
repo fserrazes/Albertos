765d5700f8947e22c16b85cdab4cc74c61e916b6
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-18 15:47:32 +0100

    Create Hippo Payments provider  to outsource the responsibility of managing payments (third-party service)

---
+++ HippoPayments/HippoPayments/HippoPaymentsError.swift
@@ -0,0 +1,6 @@
+//  Created on 18.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+public enum HippoPaymentsError: Error {
+    case genericError
+}
---
+++ HippoPayments/HippoPayments/HippoPaymentsProcessor.swift
@@ -0,0 +1,15 @@
+//  Created on 18.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import UIKit
+
+public class HippoPaymentsProcessor {
+    
+    init(apiKey: String) {
+        
+    }
+    
+    func processPayment(payload: [String : Any], onSuccess: @escaping () -> Void, onFailure: @escaping (HippoPaymentsError) -> Void) {
+        
+    }
+}
