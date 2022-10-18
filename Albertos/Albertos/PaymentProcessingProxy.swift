f5b13dfbf9aa17314210fe9fff96f73e8c5132e2
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-18 19:00:27 +0100

    Change PaymentProcessingProxy access control

--- Albertos/Albertos/PaymentProcessingProxy.swift
+++ Albertos/Albertos/PaymentProcessingProxy.swift
@@ -9,7 +9,7 @@ import HippoPayments
 /// framework in every SwiftUI view that uses. This is a workaround to the fact that
 /// `environmentObject(_:)` requires a type conforming to `ObservableObject` so we cannot pass it a
 /// value defined as `PaymentProcessing` because "only struct/enum/class types can conform to protocols".
-class PaymentProcessingProxy: PaymentProcessing, ObservableObject {
+public class PaymentProcessingProxy: PaymentProcessing, ObservableObject {
     private let proxiedProcessor: PaymentProcessing = HippoPaymentsProcessor(apiKey: "abcd")
 
     func process(order: Order) -> AnyPublisher<Void, Error> {
