4806b2d9899ba8a823babb232b171c82e91df71a
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-18 09:32:17 +0100

    Extract MenuItemDetail ViewModel to his own group folder

--- Albertos/Albertos/MenuItemDetail.swift
+++ Albertos/Albertos/MenuItemDetail.swift
@@ -2,7 +2,6 @@
 //  Copyright © 2022 Flavio Serrazes. All rights reserved.
 
 import SwiftUI
-import Combine
 import AlbertosCore
 
 struct MenuItemDetail: View {
@@ -31,53 +30,9 @@ struct MenuItemDetail: View {
     }
 }
 
-extension MenuItemDetail {
-    class ViewModel: ObservableObject {
-        private let item: MenuItem
-        private let orderController: OrderController
-        
-        let name: String
-        let spicy: String?
-        let price: String
-        
-        @Published private(set) var addOrRemoveFromOrderButtonText = "Remove from order"
-        
-        private var cancellables = Set<AnyCancellable>()
-        
-        init(item: MenuItem, orderController: OrderController) {
-            self.item = item
-            self.orderController = orderController
-            
-            name = item.name
-            spicy = item.spicy ? "Spicy" : .none
-            price = "$\(String(format: "%.2f", item.price))"
-            
-            self.orderController.$order
-                .sink { [weak self] order in
-                    guard let self = self else { return }
-
-                    if (order.items.contains { $0 == self.item }) {
-                        self.addOrRemoveFromOrderButtonText = "Remove from order"
-                    } else {
-                        self.addOrRemoveFromOrderButtonText = "Add to order"
-                    }
-                }
-                .store(in: &cancellables)
-        }
-        
-        func addOrRemoveFromOrder() {
-            if (orderController.order.items.contains { $0 == item }) {
-                orderController.removeFromOrder(item: item)
-            } else {
-                orderController.addToOrder(item: item)
-            }
-        }
-    }
-}
-
 struct MenuItemDetail_Previews: PreviewProvider {
     static var previews: some View {
-        let item = MenuItem(category: "some category", name: "any name", spicy: true, price: 0.99)
+        let item = menu.first!
         MenuItemDetail(viewModel: .init(item: item, orderController: OrderController()))
     }
 }
