496807b46d963ea29880a6f55d78f30912e19c74
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-18 09:55:41 +0100

    Test inits with empty order

--- AlbertosCore/AlbertosCoreTests/Order/Domain/OrderControllerTests.swift
+++ AlbertosCore/AlbertosCoreTests/Order/Domain/OrderControllerTests.swift
@@ -2,9 +2,14 @@
 //  Copyright © 2022 Flavio Serrazes. All rights reserved.
 
 import XCTest
+import AlbertosCore
 
 final class OrderControllerTests: XCTestCase {
+    func test_initsWithEmptyOrder() {
+        let controller = OrderController()
 
+        XCTAssertTrue(controller.order.items.isEmpty)
+    }
     
 
 }
