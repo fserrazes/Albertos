0dd0f553c2848ce41eb41684224d59ed4a9050d2
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-18 12:07:19 +0100

    Add OrderDetailViewModelTests file with Test list

---
+++ Albertos/AlbertosTests/Order Detail/Presentation/OrderDetailViewModelTests.swift
@@ -0,0 +1,24 @@
+//  Created on 18.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import XCTest
+
+final class OrderDetailViewModelTests: XCTestCase {
+
+    func test_whenOrderIsEmpty_ShouldNotShowTotalAmount() {
+        
+    }
+
+    func test_whenOrderIsNonEmpty_ShouldShowTotalAmount() {
+        
+    }
+
+    func test_whenOrderIsEmpty_HasNotItemNamesToShow() {
+        
+    }
+
+    func test_whenOrderIsNonEmpty_MenuListItemIsOrderItems() {
+        
+    }
+
+}
