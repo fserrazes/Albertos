ce1480d76ba068d43356b61dd2466649d9a1b444
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-18 11:30:01 +0100

    Add OrderButtonViewModelTests with Test list

---
+++ Albertos/AlbertosTests/Order Detail/Presentation/OrderButtonViewModelTests.swift
@@ -0,0 +1,15 @@
+//  Created on 18.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import XCTest
+
+final class OrderButtonViewModelTests: XCTestCase {
+
+    func test_whenOrderIsEmpty_doesNotShowTotal() {
+        
+    }
+
+    func test_whenOrderIsNotEmpty_showsTotal() {
+        
+    }
+}
