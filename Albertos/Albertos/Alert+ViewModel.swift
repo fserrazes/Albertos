ccc9c93893bb57aa2b277a905ffcfe96a2cbd288
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-10-18 20:37:49 +0100

    Add Alert extension helper

---
+++ Albertos/Albertos/Alert+ViewModel.swift
@@ -0,0 +1,14 @@
+//  Created on 18.10.22
+//  Copyright © 2022 Flavio Serrazes. All rights reserved.
+
+import SwiftUI
+
+extension Alert {
+    struct ViewModel: Identifiable {
+        let title: String
+        let message: String
+        let buttonText: String
+        
+        var id: String { title + message + buttonText }
+    }
+}
